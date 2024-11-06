b662d9c2cd32b15e4d0fd98caaacbee74543d455
Bun-mi <roxyperry@gmail.com>
2024-11-06 15:20:30 +0100

    :root, better color arrangement

---
+++ javascri/main.js
@@ -0,0 +1,9 @@
+// const toggleMode = document.querySelector(".tog-btn")
+
+
+// function toggleMod(){
+//     // var mainDark = document.querySelector("#main");
+//     mainDark.classList.toggle("#main");
+// }
+
+// toggleMode.addEventListener("click", toggleMod());
